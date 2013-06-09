5fbf81e94f43c610a615c855848275f516c9c232
kwaledesign <sam@kwaledesign.com>
2013-06-09 14:00:52 -0600

    rebuild docs to pull in changes made in issue #42

--- docs/project/manifest.rb
+++ docs/project/manifest.rb
@@ -1,5 +1,16 @@
+# Bower
+file 'README.md'
+
 stylesheet 'screen.scss'
-stylesheet '_settings.scss'
+
+# Base
+stylesheet 'base/_color-pallet.scss'
+stylesheet 'base/_typography-pallet.scss'
+stylesheet 'base/_page.scss'
+stylesheet 'base/_breakpoints.scss'
+stylesheet 'base/_settings.scss'
+stylesheet 'base/_structure-units.scss'
+stylesheet 'base/_skin-units.scss'
 
 # Objects
 stylesheet 'objects/_label-object.scss'
@@ -10,21 +21,17 @@ stylesheet 'objects/_nav-object.scss'
 stylesheet 'objects/_close-object.scss'
 stylesheet 'objects/_alert-object.scss'
 
-# Base
-stylesheet 'base/_color-pallet.scss'
-stylesheet 'base/_forms.scss'
-stylesheet 'base/_rules.scss'
-stylesheet 'base/_typography.scss'
-stylesheet 'base/_links.scss'
-stylesheet 'base/_lists.scss'
-stylesheet 'base/_tables.scss'
-stylesheet 'base/_media.scss'
-stylesheet 'base/_page.scss'
-stylesheet 'base/_figures.scss'
-stylesheet 'base/_code.scss'
-stylesheet 'base/_quotes.scss'
-
 # Components
+stylesheet 'components/_forms.scss'
+stylesheet 'components/_rules.scss'
+stylesheet 'components/_typography.scss'
+stylesheet 'components/_links.scss'
+stylesheet 'components/_lists.scss'
+stylesheet 'components/_tables.scss'
+stylesheet 'components/_media.scss'
+stylesheet 'components/_figures.scss'
+stylesheet 'components/_code.scss'
+stylesheet 'components/_quotes.scss'
 stylesheet 'components/_alerts.scss'
 stylesheet 'components/_labels.scss'
 stylesheet 'components/_partitions.scss'
@@ -39,6 +46,8 @@ stylesheet 'components/_buttons.scss'
 stylesheet 'layout/_print.scss'
 stylesheet 'layout/_susy-grid.scss'
 stylesheet 'layout/_singularity-grid.scss'
-#stylesheet 'layout/l-modules/_l-partitions.scss'
-#stylesheet 'layout/l-modules/_l-tabs.scss'
+
+welcome_message %Q{
+Congradulations! You've successfully installed Archetype. Please see the README.md file within your project root for additional setup information.
+}
 
